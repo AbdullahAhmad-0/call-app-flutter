156b376bf4551e51286dff77d2156049a51c6c82
Abdullah Ahmad <abdullah.devloper@gmail.com>
2025-08-01 10:48:11 +0500

    update: lib/bindings/home_binding.dart

--- lib/bindings/home_binding.dart
+++ lib/bindings/home_binding.dart
@@ -1,6 +1,6 @@
 import 'package:get/get.dart';
-import 'package:kasookoo_sdk/controllers/home_controller.dart';
-import 'package:kasookoo_sdk/controllers/call_controller.dart';
+import 'package:call_app/controllers/home_controller.dart';
+import 'package:call_app/controllers/call_controller.dart';
 
 class HomeBinding extends Bindings {
   @override
