223978ce7a5acb4bdb296a68b5ddedd46cf768b1
Abdullah Ahmad <abdullah.devloper@gmail.com>
2025-08-01 10:48:55 +0500

    update: lib/routes/app_routes.dart

---
+++ lib/routes/app_routes.dart
@@ -0,0 +1,7 @@
+abstract class Routes {
+  static const HOME = '/home';
+  static const ROLE_SELECTION = '/role-selection';
+  static const CALL_HISTORY = '/call-history';
+  static const CALLING = '/calling';
+  static const RINGING = '/ringing';
+}
