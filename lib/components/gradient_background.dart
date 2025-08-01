3c172fafedf7513aec71f2b2750267c95aaded60
Abdullah Ahmad <abdullah.devloper@gmail.com>
2025-08-01 10:48:27 +0500

    update: lib/components/gradient_background.dart

---
+++ lib/components/gradient_background.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class GradientBackground extends StatelessWidget {
+  final Widget child;
+
+  const GradientBackground({Key? key, required this.child}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            Color(0xFF1A1A2E),
+            Color(0xFF16213E),
+            Color(0xFF0F3460),
+          ],
+        ),
+      ),
+      child: child,
+    );
+  }
+}
